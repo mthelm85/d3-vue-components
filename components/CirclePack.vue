f6a616ad60c05879e96b3ccc05d9caacfe3e32ef
Matt Helm <mthelm85@gmail.com>
2019-01-21 08:08:35 -0600

    initial commit

---
+++ components/CirclePack.vue
@@ -0,0 +1,70 @@
+<template lang="html">
+  <svg :width="width" :height="height" transform="translate(10, 10)">
+    <text
+      x="50%"
+      transform="translate(0, 20)"
+      text-anchor="middle"
+      class="circle-label">
+      {{ title }}
+    </text>
+    <g opacity="0.85" transform="translate(0, 10)">
+      <circle
+        v-for="d in descendants"
+        :fill="color(d.data.name.replace(/ .*/, ''))"
+        :cx="d.x"
+        :cy="d.y"
+        :r="d.r"
+        stroke="black">
+        <title>
+          <text>{{ d.data.name }}: ${{ d.value.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}) }}</text>
+        </title>
+      </circle>
+    </g>
+  </svg>
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+  data () {
+    return {
+      descendants: null,
+      root: null
+    }
+  },
+
+  computed: {
+    color () {
+      return d3.scaleOrdinal(d3.schemeCategory10)
+    },
+    pack () {
+      return d3.pack()
+        .size([this.width, this.height])
+        .padding(this.padding)
+    }
+  },
+
+  mounted () {
+    this.root = d3.hierarchy(this.data)
+      .sum(d => d.value)
+    this.descendants = this.root.descendants()
+    this.pack(this.root)
+  },
+
+  props: [
+    'data',
+    'height',
+    'padding',
+    'width',
+    'title'
+  ]
+
+}
+</script>
+
+<style lang="css">
+.circle-label {
+  font: 22px sans-serif;
+}
+</style>
---
+++ index.js
@@ -0,0 +1,7 @@
+import CirclePack from './components/CirclePack.vue'
+
+export default {
+  install: (Vue, options) => {
+    Vue.component(CirclePack.name, CirclePack)
+  }
+}
